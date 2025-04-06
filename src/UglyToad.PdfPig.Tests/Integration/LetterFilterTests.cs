42c529ded2646d01ef9b3458ba9502bcf65b5dbc
BobLd <38405645+BobLd@users.noreply.github.com>
2025-04-06 10:32:37 +0100

    Fix CanFilterClippedLetters() failing on MacOS because font is not available

--- src/UglyToad.PdfPig.Tests/Integration/LetterFilterTests.cs
+++ src/UglyToad.PdfPig.Tests/Integration/LetterFilterTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace UglyToad.PdfPig.Tests.Integration
 {
+    using PdfPig.Fonts.SystemFonts;
     using System.Linq;
 
     public class LetterFilterTests
@@ -8,6 +9,13 @@
         public void CanFilterClippedLetters()
         {
             var one = IntegrationHelpers.GetDocumentPath("ClipPathLetterFilter-Test1.pdf");
+
+            // The 'TimesNewRomanPSMT' font is used by this particular document. Thus, results cannot be trusted on
+            // platforms where this font isn't generally available (e.g. OSX, Linux, etc.), so we skip it!
+            var font = SystemFontFinder.Instance.GetTrueTypeFont("TimesNewRomanPSMT");
+            var font1 = SystemFontFinder.Instance.GetTrueTypeFont("TimesNewRomanPS-BoldMT");
+            var font2 = SystemFontFinder.Instance.GetTrueTypeFont("TimesNewRomanPS-ItalicMT");
+            Skip.If(font is null || font1 is null || font2 is null, "Skipped because the font TimesNewRomanPSMT or a font from TimesNewRoman family could not be found in the execution environment.");
 
             using (var doc1 = PdfDocument.Open(one, new ParsingOptions { ClipPaths = true }))
             using (var doc2 = PdfDocument.Open(one, new ParsingOptions { ClipPaths = false }))
