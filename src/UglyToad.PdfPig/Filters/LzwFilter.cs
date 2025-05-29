9f16527197be95afca8b0fd52ef97c267a412ddf
BobLd <38405645+BobLd@users.noreply.github.com>
2025-05-29 14:21:31 +0100

    Update PngPredictor

--- src/UglyToad.PdfPig/Filters/LzwFilter.cs
+++ src/UglyToad.PdfPig/Filters/LzwFilter.cs
@@ -5,6 +5,7 @@ namespace UglyToad.PdfPig.Filters
     using System;
     using System.Collections.Generic;
     using Lzw;
+    using System.IO;
     using Tokens;
     using Util;
 
@@ -37,57 +38,66 @@ namespace UglyToad.PdfPig.Filters
 
             var earlyChange = parameters.GetIntOrDefault(NameToken.EarlyChange, 1);
 
-            if (predictor > 1)
-            {
-                var decompressed = Decode(input.Span, earlyChange == 1);
+            var colors = Math.Min(parameters.GetIntOrDefault(NameToken.Colors, DefaultColors), 32);
+            var bitsPerComponent = parameters.GetIntOrDefault(NameToken.BitsPerComponent, DefaultBitsPerComponent);
+            var columns = parameters.GetIntOrDefault(NameToken.Columns, DefaultColumns);
 
-                var colors = Math.Min(parameters.GetIntOrDefault(NameToken.Colors, DefaultColors), 32);
-                var bitsPerComponent = parameters.GetIntOrDefault(NameToken.BitsPerComponent, DefaultBitsPerComponent);
-                var columns = parameters.GetIntOrDefault(NameToken.Columns, DefaultColumns);
-
-                return PngPredictor.Decode(decompressed, predictor, colors, bitsPerComponent, columns);
-            }
-
-            return Decode(input.Span, earlyChange == 1);
+            return Decode(input.Span, earlyChange == 1, predictor, colors, bitsPerComponent, columns);
         }
 
-        private static byte[] Decode(ReadOnlySpan<byte> input, bool isEarlyChange)
+        private static byte[] Decode(ReadOnlySpan<byte> input, bool isEarlyChange, int predictor, int colors, int bitsPerComponent, int columns)
         {
-            // A guess.
-            var result = new List<byte>((int)(input.Length * 1.5));
-
-            var table = GetDefaultTable();
-
-            var codeBits = 9;
-
-            var data = new BitStream(input);
-
-            var codeOffset = isEarlyChange ? 0 : 1;
-
-            var previous = -1;
-
-            while (true)
+            using (var output = new MemoryStream((int)(input.Length * 1.5))) // A guess.
+            using (var result = PngPredictor.WrapPredictor(output, predictor, colors, bitsPerComponent, columns))
             {
-                var next = data.Get(codeBits);
+                var table = GetDefaultTable();
 
-                if (next == EodMarker)
-                {
-                    break;
-                }
-                
-                if (next == ClearTable)
-                {
-                    table = GetDefaultTable();
-                    previous = -1;
-                    codeBits = 9;
-                    continue;
-                }
+                var codeBits = 9;
 
-                if (table.TryGetValue(next, out var b))
-                {
-                    result.AddRange(b);
+                var data = new BitStream(input);
 
-                    if (previous >= 0)
+                var codeOffset = isEarlyChange ? 0 : 1;
+
+                var previous = -1;
+
+                while (true)
+                {
+                    var next = data.Get(codeBits);
+
+                    if (next == EodMarker)
+                    {
+                        break;
+                    }
+
+                    if (next == ClearTable)
+                    {
+                        table = GetDefaultTable();
+                        previous = -1;
+                        codeBits = 9;
+                        continue;
+                    }
+
+                    if (table.TryGetValue(next, out var b))
+                    {
+                        foreach (var b1 in b)
+                        {
+                            result.WriteByte(b1);
+                        }
+
+                        if (previous >= 0)
+                        {
+                            var lastSequence = table[previous];
+
+                            var newSequence = new byte[lastSequence.Length + 1];
+
+                            Array.Copy(lastSequence, newSequence, lastSequence.Length);
+
+                            newSequence[lastSequence.Length] = b[0];
+
+                            table[table.Count] = newSequence;
+                        }
+                    }
+                    else
                     {
                         var lastSequence = table[previous];
 
@@ -95,47 +105,40 @@ namespace UglyToad.PdfPig.Filters
 
                         Array.Copy(lastSequence, newSequence, lastSequence.Length);
 
-                        newSequence[lastSequence.Length] = b[0];
+                        newSequence[lastSequence.Length] = lastSequence[0];
+
+                        foreach (var b1 in newSequence)
+                        {
+                            result.WriteByte(b1);
+                        }
 
                         table[table.Count] = newSequence;
                     }
-                }
-                else
-                {
-                    var lastSequence = table[previous];
 
-                    var newSequence = new byte[lastSequence.Length + 1];
+                    previous = next;
 
-                    Array.Copy(lastSequence, newSequence, lastSequence.Length);
+                    if (table.Count >= ElevenBitBoundary + codeOffset)
+                    {
+                        codeBits = 12;
+                    }
+                    else if (table.Count >= TenBitBoundary + codeOffset)
+                    {
+                        codeBits = 11;
+                    }
+                    else if (table.Count >= NineBitBoundary + codeOffset)
+                    {
+                        codeBits = 10;
+                    }
+                    else
+                    {
+                        codeBits = 9;
+                    }
+                }
 
-                    newSequence[lastSequence.Length] = lastSequence[0];
-
-                    result.AddRange(newSequence);
-
-                    table[table.Count] = newSequence;
-                }
-                
-                previous = next;
-
-                if (table.Count >= ElevenBitBoundary + codeOffset)
-                {
-                    codeBits = 12;
-                }
-                else if (table.Count >= TenBitBoundary + codeOffset)
-                {
-                    codeBits = 11;
-                }
-                else if (table.Count >= NineBitBoundary + codeOffset)
-                {
-                    codeBits = 10;
-                }
-                else
-                {
-                    codeBits = 9;
-                }
+                //result.CopyTo(f);
+                result.Flush();
+                return output.ToArray();
             }
-
-            return result.ToArray();
         }
 
         private static Dictionary<int, byte[]> GetDefaultTable()
